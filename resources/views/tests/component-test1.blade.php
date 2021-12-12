b74d9ce854e0178167588a932730ff1f3c50232c
dt819 <shukriya.819@gmail.com>
2021-12-12 18:18:45 +0900

    attributes

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     コンポーネントテスト1
     <x-tests.card title="タイトル1" content="本文" :message="$message"></x-tests.card>
     <x-tests.card title="タイトル2"></x-tests.card>
+    <x-tests.card title="スペシャルタイトル" class="bg-red-300"></x-tests.card>
 </x-tests.app>
